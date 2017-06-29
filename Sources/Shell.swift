ec6c028a9fd934393cdb8d539279b0862909f410
Jeremy Greenwood <jeremy@mobelux.com>
2017-06-29 13:45:53 -0400

    executing command

--- Sources/Shell.swift
+++ Sources/Shell.swift
@@ -1,4 +1,21 @@
-struct Shell {
+import Foundation
 
-    var text = "Hello, World!"
+public struct Shell {
+    public static func execute(command: String, arguments: [String]? = nil) -> String {
+        let process = Process()
+        process.launchPath = command
+        process.arguments = arguments
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        process.launch()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+
+        guard let output: String = String(data: data, encoding: String.Encoding.utf8) else {
+            return ""
+        }
+
+        return output
+    }
 }
